307276e65334ed812fc2fe974fd4d94afd0a21f1
Clovis Tricon <clovistricon@localhost.localdomain>
2019-02-13 12:55:25 +0100

    AI and norm(2)

--- AI_strat.c
+++ AI_strat.c
@@ -17,7 +17,7 @@ int two_l(int line1, int line2, int limit)
         return (line2 - n);
     if (diff == ((limit - n) + 1))
         return (line1 - 1);
-    if ((diff > ((limit - n) + 1)) && (diff < (((limit * 2) - n) + 1)));
+    if ((diff > ((limit - n) + 1)) && (diff < (((limit * 2) - n) + 1)))
         return (line2 + ((limit - n) + 1));
     else if (diff >= (((limit * 2) - n) + 1)) {
         if (diff == (limit * 2))
@@ -64,37 +64,49 @@ int *win_in_2_lines(int *board_game, int limit)
     return (board_game);
 }
 
+int *win_in_2lines_and_1matches(int *board_game, int nb, int lmax2, int limit)
+{
+    int lmax = 0;
+    int l_1 = 0;
+    int diff;
+
+    for (int i = 0; i < nb; i = i + 1) {
+        lmax = ((board_game[i] > board_game[lmax]) ? (i) : (lmax));
+        l_1 = ((board_game[i] == 1) ? (i) : (l_1));
+    }
+    diff = board_game[lmax] - board_game[lmax2];
+    if ((diff <= 1) || (diff == (limit + 2)))
+        board_game[l_1] = 0;
+    else {
+        if (diff <= (limit + 1))
+            board_game[lmax] = board_game[lmax2] + 1;
+        else if ((diff > (limit + 1)) && (diff <= ((limit * 2) + 1)))
+            board_game[lmax] = board_game[lmax2] + (limit + 2);
+        else
+            board_game[lmax] = board_game[lmax] - limit;
+    }
+    return (board_game);
+}
+
 int *win_in_3_lines(int *board_game, int nb, int limit)
 {
     int lines_1 = 0;
-    int l_1 = 0;
     int lmax = 0;
     int lmax2 = 0;
-    int diff;
 
     for (int i = 0; i < nb; i = i + 1) {
         lines_1 = ((board_game[i] == 1) ? (lines_1 + 1) : (lines_1));
         lmax = ((board_game[i] > board_game[lmax]) ? (i) : (lmax));
-        l_1 = ((board_game[i] == 1) ? (board_game[i]) : (l_1));
     }
     for (int a = 0; a < nb; a = a + 1) {
         if (a != lmax)
             lmax2 = ((board_game[a] > board_game[lmax2]) ? (a) : (lmax2));
     }
     if (lines_1 >= 2)
-        too_many_1_matches_line(board_game, nb, limit);
-    else if (lines_1 == 1) {
-        diff = board_game[lmax] - board_game[lmax2];
-        if ((diff == 0) || (diff == limit))
-            board_game[l_1] = 0;
-        else if (diff == 1)
-            board_game[lmax] = board_game[lmax] - 1;
-        else if ((diff > 1) && (diff <= limit))
-            board_game[lmax] = board_game[lmax] - (diff - 1);
-        else
-            board_game[lmax] = board_game[lmax] - limit;
-    }
+        board_game = too_many_1_matches_line(board_game, nb, limit);
+    else if (lines_1 == 1)
+        board_game = win_in_2lines_and_1matches(board_game, nb, lmax2, limit);
     else
-        random_line(board_game, nb, limit);
+        board_game = random_line(board_game, nb, limit);
     return (board_game);
 }
--- my.h
+++ my.h
@@ -32,6 +32,7 @@ int *random_line(int *board_game, int nb, int limit);
 int *win_in_1_line(int *board_game, int limit);
 int two_l(int line1, int line2, int limit);
 int *win_in_2_lines(int *board_game, int limit);
+int *win_in_2lines_and_1matches(int *board_game, int nb, int lmax2, int limit);
 int *win_in_3_lines(int *board_game, int nb, int limit);
 int *ai_turn(int nb, int limit, int *board_game);
 int give_result(int turn);
